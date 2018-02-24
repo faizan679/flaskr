4bdef7f7d0e41b2faad529b88e5155a270fe2d13
faizan679 <khanfaizan679@gmail.com>
2018-02-24 18:47:57 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+"""this is an expanded dictionary using = symbol"""
+setup(
+	name = 'flaskr',
+	packages=['flaskr'],
+	include_package_data = True,
+	install_requires=['flask',],
+)
